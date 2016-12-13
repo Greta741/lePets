6e2509fcd5682f513f414af1812a17a2eed8d177
Laurynas Mekšraitis <laurynasmeksraitis@gmail.com>
2016-12-14 00:31:53 +0200

    Veislių sąrašas.

--- services/veisliuValdiklis.js
+++ services/veisliuValdiklis.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const vartotojai = require('./vartotojuValdiklis.js');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -8,52 +9,26 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-let htmlData = {};
-htmlData.head = '<head><title>Le pets</title>' +
-    '<link rel="stylesheet" href="../public/CSS/styles.css">' +
-    '<meta charset="utf-8">' +
-    '<meta name="viewport" content="width=device-width, initial-scale=1">' +
-    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">' +
-    '<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">' +
-    '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>' +
-    '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script></head>';
-htmlData.navbar = '<nav class="navbar navbar-default"><div class="container-fluid">' +
-    '<div class="navbar-header"><a class="navbar-brand" href="/">Le pets</a></div>' +
-    '<ul class="nav navbar-nav">' +
-      '<li><a href="/veislynas/11">Veislynai</a></li>' +
-      '<li><a href="/gyvunai">Gyvūnai</a></li>' +
-      '<li><a href="/veisle/1">Veislės</a></li>' +
-      '<li><a href="#"><span class="glyphicon glyphicon-search"></span> Paieška</a></li>'+ 
-    '</ul>' +
-    '<ul class="nav navbar-nav navbar-right">' +
-    '<li class="dropdown">' +
-        '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Ataskaitos<span class="caret"></span></a>' +
-        '<ul class="dropdown-menu">' +
-          '<li><a href="#">Veislynų ataskaita</a></li>' +
-          '<li><a href="#">Veislių ataskaita</a></li>' +
-          '<li><a href="#">Gyvūnų ataskaita</a></li>' +
-          '<li><a href="#">Vartotojų ataskaita</a></li>' +
-        '</ul></li>' +
-    '<li class="dropdown">' +
-        '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Menu<span class="caret"></span></a>' +
-        '<ul class="dropdown-menu">' +
-          '<li><a href="#">Redaguoti profilį</a></li>' +
-          '<li><a href="#">Keisti roles</a></li>' +
-          '<li><a href="#">Patvirtinti veislynus</a></li>' +
-          '<li><a href="#">Žinutės</a></li>' +
-          '<li><a href="#">Prenumerata</a></li>' +
-          '<li><a href="#">Atsijungti</a></li>' +
-        '</ul></li>' +
-    '<li><a href="./register"><span class="glyphicon glyphicon-user"></span> Registruotis</a></li>' +
-    '<li><a href="./login"><span class="glyphicon glyphicon-log-in"></span> Prisijungti</a></li>' +
-    '</ul></div></nav>';
-
-const chooseTypeView = (request, reply) => {
-  reply.view('./veisles/veisTipoPasirinkimas.html', {htmlData});
+const allBreeds = (request, reply) => {
+  const data = {};
+  connection.query('select * from veisle', (err, veisles) => {
+      if (veisles.length === 0) {
+        data.message = 'Veislių sistemoje nėra.'; 
+        reply.view('./veisles/visosVeisles.html', {htmlData: vartotojai.generateNavBar(request.state.session), data});
+        return;       
+      }
+      else {
+        data.veisles = veisles;
+        data.veisles.forEach((item) => {
+          item.redagavimo_data = formatDate(item.gimimo_data);
+        });
+        reply.view('./veisles/visosVeisles.html', {htmlData: vartotojai.generateNavBar(request.state.session), data});
+      }      
+    });
 };
 
 const registerView = (request, reply) => {
-  reply.view('./veisles/veislesRegistracija.html', {htmlData});
+  reply.view('./veisles/veislesRegistracija.html', {htmlData: vartotojai.generateNavBar(request.state.session)});
 };
 
 const insertNew = (data) => {
@@ -93,12 +68,12 @@ const showPage = (request, reply) => {
     data.veisle.registravimo_data = formatDate(data.veisle.registravimo_data);
     data.veisle.redagavimo_data = formatDate(data.veisle.redagavimo_data);
     // data.image = `<img class="v-image" src="${veisle[0].nuotraukos_url}" alt="img"></img>`
-    reply.view('./veisles/veisle.html', {htmlData, data});
+    reply.view('./veisles/veisle.html', {htmlData: vartotojai.generateNavBar(request.state.session), data});
   });
 }
 
 module.exports = {
-  chooseTypeView,
+  allBreeds,
   registerView,
   insertNew,
   showPage,
